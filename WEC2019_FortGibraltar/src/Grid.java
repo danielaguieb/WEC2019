7a44d6ccb16c3a01ccda0202c363fc70ac7b57bb
Daniel Guieb <35285976+danielaguieb@users.noreply.github.com>
2019-01-18 09:58:29 -0700

    Update Grid.java

--- WEC2019_FortGibraltar/src/Grid.java
+++ WEC2019_FortGibraltar/src/Grid.java
@@ -2,39 +2,52 @@ import java.util.Random;
 import java.lang.Math;
 
 public class Grid {
-
-	// 1 is basin, 0 is clear
 	public Tile[][] theGrid;
-	public int gridBounds;
+	public int size;
 	public int num_basins;
 	
-	public Grid (int size) {
-		theGrid = new Tile[size][size];
-		gridBounds = size;
+	public Grid (int n) {
+		theGrid = new Tile[n][n];
+		this.size = n;
+		num_basins = (int) Math.sqrt(n);
 		randomizeBasins();
 	}
 	
 	private void randomizeBasins() {
-		
+		Random random = new Random();
+		int rand_row = random.nextInt(size) + 1;
+		int rand_col = random.nextInt(size) + 1;
+
+		for(int i=0; i<num_basins; i++) {
+			while(theGrid[rand_row][rand_col].hasBasin()) {
+				rand_row = random.nextInt(size) + 1;
+				rand_col = random.nextInt(size) + 1;
+			}
+			
+			theGrid[rand_row][rand_col].setBasin(true);
+		}
 	}
 	
 	public int check(int row, int col) {
 		theGrid[row][col].setClicked(true);
-		
-		if(theGrid[row][col].hasBasin())         
-			// if chosen tile has a basin, immediate failure
+
+		// if chosen tile has a basin, immediate failure
+		if(theGrid[row][col].hasBasin())
 			return -1;
-		else {
-			int basinCounter = 0;
-			for(int N=row-1; N<3; N++) {
-				for(int M=col-1; M<3; M++) {
-					if(N>=0 && N < gridBounds && M>=0 && M < gridBounds) {
-						if(theGrid[N][M].hasBasin())
-							basinCounter++;
-					}
+		
+		int basinCounter = 0;
+		for(int N=row-1; N<3; N++) {
+			for(int M=col-1; M<3; M++) {
+				if(N>=0 && N < size && M>=0 && M < size) {
+					if(theGrid[N][M].hasBasin())
+						basinCounter++;
 				}
 			}
-			return basinCounter;
 		}
+		return basinCounter;	
 	}
+	
+	
+	
+	
 }
